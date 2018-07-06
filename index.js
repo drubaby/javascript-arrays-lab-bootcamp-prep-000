0ed754e12371921117718c536d2b539b758820e7
Dru Edmondson <peryton6@gmail.com>
2018-07-06 15:51:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function destructivelyAppendKitten(name){
 function destructivelyPrependKitten(name){
   return kittens.unshift(name);
 }
+
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop(name);
+}
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift(name);
+}
